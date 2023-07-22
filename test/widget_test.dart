04d053043b532c2182e7db11d48709eeaa086ce6
SumeyyeAydogan <aydogannsumeyye@gmail.com>
2023-07-22 15:42:42 +0300

    second

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:input_learn/main.dart';
+import 'package:input_widget_learning/main.dart';
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
